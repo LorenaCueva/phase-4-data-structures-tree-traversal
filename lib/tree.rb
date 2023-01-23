186429f6e5d6e369afae6fe6bd9ddedeabe937c7
Lorena Cueva <lorenacueva@gmail.com>
2023-01-23 15:27:05 -0800

    commit tree 32896bfcbc6a8b8da29518a1ab9e12f7cbbced0a

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,43 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  # def breath_first_traversal(node)
+  #   result = []
+  #   nodes_to_visit = [node]
+
+  #   while nodes_to_visit.length > 0
+
+  #     node = nodes_to_visit.shift
+  #     result.push(node[:value])
+  #     nodes_to_visit = nodes_to_visit + node[:children]
+
+  #   end
+  #   result
+  # end
+
+  # def depth_first_traversal(node)
+  #   result = []
+  #   nodes_to_visit = [node]
+
+  #   while nodes_to_visit.length > 0 
+  #     node = nodes_to_visit.shift
+  #     result.push(node[:value])
+  #     nodes_to_visit = node[:children] + nodes_to_visit
+  #   end
+
+  #   result
+  # end
+
+  def get_element_by_id(id)
+    nodes_to_visit = [@root]
+    
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      return node if node[:id] == id
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+    nil
+  end 
+
 end
